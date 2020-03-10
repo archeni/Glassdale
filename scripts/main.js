c74d0aa624eb3b7910cb0688f3afcc3a768718d7
archeni <johnson.traye@yahoo.com>
2020-03-10 13:15:12 -0500

    did a lot

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
 import { criminalMaker } from "./criminals/criminalsList.js";
 import { getcriminals } from "./criminals/criminalsProvider.js";
+import { getConvictions } from "./convictions/convictionProvider.js";
+import convictionMaker from "./convictions/convictionSelect.js";
 
-getcriminals().then(() => criminalMaker())
+getcriminals().then(criminalMaker)
+
+getConvictions().then(convictionMaker)
